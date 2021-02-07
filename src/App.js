471636f25092d832b6c48fb4a3952607d12969e7
saravindswamy <75692841+saravindswamy@users.noreply.github.com>
2021-02-07 18:41:11 +0530

    code cleaning

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import axios from "axios";
+import classes from "./App.css";
+const useStyles = makeStyles({
+  table: {
+    minWidth: 300,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function App() {
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const classes = useStyles();
+
+  const getProductData = async () => {
+    try {
+      const data = await axios.get(
+        "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+      );
+
+      console.log(data.data);
+      setProducts(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  return (
+    <div>
+      <br />
+      <div className="App">
+        <h3>Enter the product name to get the price details</h3>
+        <input
+          type="text"
+          placeholder="Search.."
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
+
+        <br />
+        <TableContainer component={Paper}>
+          <Table
+            className={classes.table}
+            size="small"
+            aria-label="a dense table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell>Product Name</TableCell>
+                <TableCell align="left">Product Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products
+                .filter((item) => {
+                  if (searchTerm == "") {
+                    return null;
+                  } else if (
+                    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+                  ) {
+                    return item;
+                  }
+                })
+                .map((item) => {
+                  return (
+                    <TableRow key={item.name}>
+                      <TableCell component="th" scope="row">
+                        {item.name}
+                      </TableCell>
+                      <TableCell align="left">{item.price}</TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+}
